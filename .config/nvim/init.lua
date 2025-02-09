150e3820985959ddd5167c59a1b72428240d3513
nilsmuralles <murallesnils11@gmail.com>
2025-02-09 17:19:12 -0600

    Refactorizando configuracion de NeoVim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,90 +1,17 @@
-require("config.lazy")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+local opts = {}
+
+require("vim-options")
 require("lazy").setup("plugins")
 
--- Colorscheme
-vim.cmd.colorscheme("catppuccin")
-
--- Telescope config
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<C-f>", builtin.live_grep, { desc = "Telescope live grep" })
-
--- Treesitter configuration
-local config = require("nvim-treesitter.configs")
-config.setup({
-	auto_install = true,
-	highlight = { enabled = true },
-	indent = { enabled = true },
-})
-
--- Configure shortcut for opening Neotree
-vim.keymap.set("n", "<C-b>", ":Neotree")
-
--- Make Lualine start automatically
-require("lualine").setup()
-
--- Configuration of LSP and servers
-require("mason").setup()
-require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls" },
-})
-
--- LSP servers
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local lspconfig = require("lspconfig")
-lspconfig.lua_ls.setup({
-	capabilities = capabilities
-})
-
--- None-ls
-local null_ls = require("null-ls")
-
-null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.stylua,
-	},
-})
-vim.keymap.set("n", "<C-r>", vim.lsp.buf.format, {})
-
--- Completions
-local cmp = require("cmp")
-require("luasnip.loaders.from_vscode").lazy_load()
-
-cmp.setup({
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-			-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-
-			-- For `mini.snippets` users:
-			-- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
-			-- insert({ body = args.body }) -- Insert at cursor
-			-- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
-			-- require("cmp.config").set_onetime({ sources = {} })
-		end,
-	},
-	window = {
-		 completion = cmp.config.window.bordered(),
-		 documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	sources = cmp.config.sources({
-		-- { name = "nvim_lsp" },
-		-- { name = "vsnip" }, -- For vsnip users.
-		{ name = 'luasnip' }, -- For luasnip users.
-		-- { name = 'ultisnips' }, -- For ultisnips users.
-		-- { name = 'snippy' }, -- For snippy users.
-	}, {
-		{ name = "buffer" },
-	}),
-})
