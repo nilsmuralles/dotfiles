7072fbf693d06b913eeb9b33576a94e2091337ca
nilsmuralles <murallesnils11@gmail.com>
2025-02-09 19:30:26 -0600

    Abreviaciones emmet no funcionan

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,38 +1,57 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-		  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local emmet_capabilities = vim.lsp.protocol.make_client_capabilities()
+      emmet_capabilities.textDocument.completion.completionItem.snippetSupport = true
       local lspconfig = require("lspconfig")
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
       lspconfig.lua_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+      })
+      lspconfig.emmet_ls.setup({
+        capabilities = emmet_capabilities,
+        filetypes = {
+          "css",
+          "eruby",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "less",
+          "sass",
+          "scss",
+          "svelte",
+          "pug",
+          "typescriptreact",
+          "vue",
+        },
+        init_options = {
+          html = {
+            options = {
+              ["bem.enabled"] = true,
+            },
+          },
+        },
       })
 
-			vim.keymap.set("n", "I", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "I", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -4,8 +4,11 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+        -- lua
 				null_ls.builtins.formatting.stylua,
+        -- javascript
         null_ls.builtins.formatting.prettier,
+        -- python
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
 			},
